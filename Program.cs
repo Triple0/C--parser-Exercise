f2b88e2129b2caa0b5e2c194a96bca51d274dafa
Triple0 <lekan.olapeju@yahoo.com>
2020-05-11 06:52:02 +0100

    Added comments

--- Program.cs
+++ Program.cs
@@ -5,42 +5,56 @@ namespace C__parser
 {
     class Program
     {
+        // A Method that handles both silent running and verbose running of replaceOperation method.
         static void displayOptions(string path)
         {
-            Console.WriteLine(path);
-            string oldFileContent = File.ReadAllText(path);
-            Console.WriteLine("Instruction: Enter 1 for program to output your operations to screen and 0 not to output, please! NOTE - Only use option 1 if file content is not large!");
-            Console.WriteLine("");
-            Console.WriteLine("Enter your choice, please: ");
-            string displayOptionChoice = Console.ReadLine();
-
-            if (displayOptionChoice == "0")
+            if (path == "")
             {
-                ReplaceOperation(path);
-            }
-            else if (displayOptionChoice == "1")
-            {
-                Console.WriteLine($"File Content: {oldFileContent}");
-                Console.WriteLine("");
-                ReplaceOperation(path);
-                Console.WriteLine(File.ReadAllText(path));
-                Console.WriteLine("");
+                Console.WriteLine("Error!! No path specified! Kindly select a filename first");
             }
             else
             {
-                Console.WriteLine($"Error! Option {displayOptionChoice} selected does not exist!");
+                string oldFileContent = File.ReadAllText(path);
+                Console.WriteLine("Instruction: Enter 1 for program to output your operations to screen and 0 not to output, please! NOTE - Only use option 1 if file content is not large!");
                 Console.WriteLine("");
+                Console.WriteLine("Enter your choice, please: ");
+                string displayOptionChoice = Console.ReadLine();
+
+
+                if (displayOptionChoice == "0") //Handles silent running
+                {
+                    ReplaceOperation(path);
+                }
+                else if (displayOptionChoice == "1") //Handles verbose running
+                {
+                    Console.WriteLine($"File Content: {oldFileContent}");
+                    Console.WriteLine("");
+                    ReplaceOperation(path);
+                    Console.WriteLine(File.ReadAllText(path));
+                    Console.WriteLine("");
+                }
+                else //Handles options that are not correct
+                {
+                    Console.WriteLine($"Error! Option {displayOptionChoice} selected does not exist!");
+                    Console.WriteLine("");
+                }
             }
         }
+
+        // A Method that performs replacement operation including reading content from and to specified file.
         static void ReplaceOperation(string path)
         {
             Console.WriteLine("Enter the string to be replaced, please: ");
             string userInput = Console.ReadLine();
+            Console.WriteLine("");
+            Console.WriteLine("Enter the string to replace your string, please: ");
+            string userReplaceInput = Console.ReadLine();
+            Console.WriteLine("");
             // Reference: https://stackoverflow.com/questions/13509532/how-to-find-and-replace-text-in-a-file-with-c-sharp
             if (File.Exists(path))
             {
                 string initialFIleContent = File.ReadAllText(path);
-                string finalFileContent = initialFIleContent.Replace(userInput, "");
+                string finalFileContent = initialFIleContent.Replace(userInput, userReplaceInput);
                 File.WriteAllText(path, finalFileContent);
 
             }
@@ -52,8 +66,11 @@ namespace C__parser
 
         static void Main(string[] args)
         {
+            // Declaring global variables
+            string filename = "";
+            string path = "";
             string counter = "0";
-            while (counter != "3")
+            while (counter != "3") // initialising a while that only terminates by user input
             {
                 Console.WriteLine("****************************************************************************");
                 Console.WriteLine("*                                                                          *");
@@ -65,22 +82,20 @@ namespace C__parser
                 Console.WriteLine("*                    Select 2 to enter a word                              *");
                 Console.WriteLine("*                   Select 3 to terminate/exit                             *");
                 Console.WriteLine("****************************************************************************");
-                Console.WriteLine("Welcome. Enter an option, please: ");
+                Console.WriteLine("Enter an option, please: ");
                 string option = Console.ReadLine();
-                Console.WriteLine(""); 
-                string filename = "filename.txt";
-                string path = @"C:\Users\Olalekan\Documents\TechCareer\Assignment\C#-parser\filename.txt"; 
+                Console.WriteLine("");
 
+                // Handles which operation to be carried out based on users' options
                 switch (option)
                 {
                     case "1":
                         Console.WriteLine("Enter your filename, please: ");
                         filename = Console.ReadLine();
                         Console.WriteLine(filename);
-                        path = Path.Combine(Environment.CurrentDirectory, filename);    
+                        path = Path.Combine(Environment.CurrentDirectory, filename);
                         break;
                     case "2":
-                    Console.WriteLine(path); 
                         displayOptions(path);
                         break;
                     case "3":
@@ -88,6 +103,8 @@ namespace C__parser
                         counter = "3";
                         break;
                     default:
+                        Console.WriteLine($"Warning! You entered {option}. You can only choice options 1, 2 and 3. Please refer to program instruction. ");
+                        Console.WriteLine("");
                         break;
 
                 }
